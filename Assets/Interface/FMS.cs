e2f70d64c74c1a65d9e8bbd80ee032061932c2da
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-09-08 15:17:27 +0900

    commit
    test

---
+++ Assets/Interface/FMS.cs
@@ -0,0 +1,14 @@
+﻿namespace Assets.Code.Interfaces
+{
+ // IStateを実装したクラスは、IState型として利用・
+ // 活用ができるようになります。
+ public interface FMS
+	
+ {
+  // UnityのUpdate()メソッドが呼び出します。
+  void StateUpdate();
+ 
+  // 描画に使います。
+  void Render();
+ }
+}
