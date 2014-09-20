0a462a65f1bdf613a6791d21c783fa02cba50ec2
TomohikoT <idumi_toilet@yahoo.co.jp>
2014-09-20 20:56:53 +0900

    フォルダ追加

---
+++ Assets/Scripts/SM.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Assets.Code.States;
+using Assets.Code.Interfaces;
+ 
+public class SM : MonoBehaviour
+{
+ 
+	[HideInInspector]
+
+ private FMS activeState;
+
+ public static SM instance;
+ 
+ void Awake()
+ {
+ 
+  if(instance == null) {
+   instance = this;
+   DontDestroyOnLoad(gameObject);
+  } else {
+   DestroyImmediate(gameObject);
+  }
+ 
+ }
+ 
+ void OnGUI()
+ {
+   activeState.Render();
+ }
+ 
+ void Start()
+  {
+    activeState = new TitleState(this);
+  //  gameData = GetComponent<GameData> ();
+  }
+ void Update()
+ {
+  if(activeState != null)
+   activeState.StateUpdate();
+ }
+ public void SwitchState(FMS newState) 
+ {
+  activeState = newState;
+ }
+}
---
+++ Assets/States/TitleState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Assets.Code.Interfaces;
+ 
+namespace Assets.Code.States {
+ public class TitleState : FMS { 
+  
+  private SM manager;
+		
+ 
+  public TitleState(SM SM) { 
+   //初期化
+   manager = SM;
+  }
+ 
+  public void StateUpdate() { 
+   //更新処理
+   if(Input.GetKeyUp(KeyCode.Return)) { // Returnキーを押すと遷移
+    Application.LoadLevel("Scene2");//シーン0に移る
+    Debug.Log("Play State");
+  //  manager.SwitchState(new RenderState(manager));
+   }
+  }
+ 
+  public void Render() { //エラー
+   //描画等
+  }
+ }
+ 
+}
